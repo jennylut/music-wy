aeb6edb621d3745fe1b29988a30696c5ed5da84d
gyz <gyz@xianjinxia.com>
2017-12-01 18:51:29 +0800

    staic page

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 ## React creat-react-app 脚手架搭建环境步骤
 
-1.npm install -g create-react-app  <!-- 全局安装creat-react-app -->
+1. npm install -g create-react-app  ··· 全局安装creat-react-app ···
 
-2.create-react-app myapp  <!-- 使用命令创建应用，myapp为项目名称 -->
+2. create-react-app myapp  ··· 使用命令创建应用，myapp为项目名称 ···
 
-3. cd myapp   <!-- 进入目录，然后启动  -->
+3. cd myapp   ··· 进入目录，然后启动  ···
 
 4. npm start
 
 
-## 更改默认端口号- 进入node-modules文件夹找到start.js,找到一下两行
+## 更改默认端口号- 进入node-modules文件夹找到start.js,找到以下两行
 const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3300;
 const HOST = process.env.HOST || '0.0.0.0';
 
--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="bg">
+          <div className="logo"></div>
+          <div className="m-song-wrap">
+            <div className="m-song-disc">
+              <div className="m-song-turn">
+                <div className="m-song-rollwrap"></div>
+                <div className="m-song-lgour"></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
